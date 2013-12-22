3ecc8e64ba0f9b4c2042e5ab2986125264569e98
satoshihaga3104 <satoshi.haga@catalinamarketing.com>
2013-12-23 02:22:24 +0900

    Salesforce Main Class

---
+++ Selenium2SampleByMave/src/test/java/com/example/salesforce/Main/Main.java
@@ -0,0 +1,30 @@
+package com.example.salesforce.Main;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+ 
+public class Main  {
+	public static void main(String[] args) {
+
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("Cheese!");
+		element.submit();
+ 
+		System.out.println("Page title is: " + driver.getTitle());
+		(new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver d) {
+				return d.getTitle().toLowerCase().startsWith("cheese!");
+			}
+		});
+ 
+		System.out.println("Page title is: " + driver.getTitle());
+		driver.quit();
+	}
+}
